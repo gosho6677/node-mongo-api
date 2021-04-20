6f8f4984b218bb6baa1593b974fe3cd40edbed46
gosho6677 <59176653+gosho6677@users.noreply.github.com>
2021-04-20 21:36:33 +0300

    todo: Controllers

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./app/models');
+const app = express();
+
+let corsOptions = {
+  origin: 'http://localhost:8081'
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log(`Connected to the db.`);
+  })
+  .catch(err => {
+    console.log(`Can't connect to the db!`, err);
+    process.exit();
+  })
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running on ${PORT} port`));
